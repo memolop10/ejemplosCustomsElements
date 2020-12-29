794873183f50df596d1e3623d1871ce8d3ed8d5e
memolop10 <inglopezvargas@gmail.com>
2020-12-29 10:16:21 -0600

    prueba de templates en lit-element

---
+++ lit-element-starter-js-master/performant-template.js
@@ -0,0 +1,50 @@
+import {LitElement, html, css} from 'lit-element';
+
+
+export class PerformantTemplate extends LitElement {
+  static get styles() {
+    return css`
+      :host{
+        --color-component:#3950cfdd;
+      }
+      .myBool{
+        color:var(--color-component,#811b5f);
+      }
+
+      p,span{
+        color:var(--color-component);  
+      }
+
+    `;
+  }
+
+  static get properties() {
+    return {
+     myString:String,
+     myArray:Array,
+     myBoolean:Boolean
+    };
+  }
+
+  constructor() {
+    super();
+    this.myString = 'Hola MEMO';
+    this.myArray = ['este','es','mi','Array'];
+    this.myBoolean = true;
+  }
+
+  render() {
+    return html`
+     <span>${this.myString}</span>
+
+     ${this.myArray.map(i => html`<p>${i}</p>`)}
+
+     ${this.myBoolean?
+        html`<p class="myBool">Me rendereo cuando Soy TRUE</p>`:
+        html`<p class="myBool">Me rendereo cuando Soy FALSE</p>`
+     }
+    `;
+  }
+}
+
+window.customElements.define('performant-template', PerformantTemplate);
